6f9f159f3d8ea7fb402849844e7a1cc2b3ddaff6
Sigma-cc <77269679+Sigma-cc@users.noreply.github.com>
2021-02-07 17:01:20 +0200

    Update rpc-module.js

--- scripts/rpc-module.js
+++ scripts/rpc-module.js
@@ -1,57 +1,145 @@
+  
 const rpc = require("discord-rpc")
 const rpc_client = new rpc.Client({
     transport: 'ipc'
 })
-
 const $ = require( "jquery" );
-function on_update(){
 
+function openAttachment() {
+    document.getElementById('attachment').click();
+}
+
+function fileSelected(input){
+    let file = input.files[0];
+    let reader = new FileReader()
+    reader.onload = () =>  {
+        let settings = JSON.parse(reader.result);
+        $('#app_id').val(settings['pid']);
+        $('#details').val(settings["details"]);
+        $('#state').val(settings["state"]);
+        $('#start_time').val(settings["timestamps"]['start']);
+        $('#end_time').val(settings["timestamps"]['end']);
+        $('#large_image_asset').val(settings["assets"]['large_image']);
+        $('#large_image_text').val(settings["assets"]['large_text']);
+        $('#small_image_asset').val(settings["assets"]['small_image']);
+        $('#small_image_text').val(settings["assets"]['small.text']);
+        $('#buttons').children('div').remove();
+        if (settings['buttons'] != undefined){
+            settings['buttons'].forEach(button => {
+                $("#buttons").append(`        <div id = "button">
+                <label>Label:</label>
+                <input placeholder="Text" value = "${button['label']}"><br>
+                <label>Link:</label>
+                <input placeholder="Link" value = "${button['url']}"><br>
+                <button type="button" class="btn btn-outline-danger my-0 my-sm-0" onclick="remove_button(this)" id = "rpc-button">Remove</button><br>
+              </div>
+              `);
+            })
+
+        }
+    }
+    reader.readAsText(file);
+}
+
+function save_settings() {//https://stackoverflow.com/questions/13405129/javascript-create-and-save-file
+    var data = JSON.parse(JSON.stringify(get_presence()));
+    data['pid'] = $('#app_id').val().toString();
+    data = JSON.stringify(data);
+
+    var file = new Blob([data], {type: 'application/json'});
+    var filename = 'settings.json';
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+        url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
+function get_presence(){
+    function validator(area_value){
+        return `${$(`#${area_value.id}`).val().toString()}`;
+    }
+    function valid(area_value){
+        return (`${$(`#${area_value.id}`).val().toString().replace(/\s/g, "")}` != '');
+    }
     var presense = {assets : {}, timestamps : {}};
 
-    function value_id(id){
-        return `${$(`#${id}`).val().toString()}`
-    }
-    function value_id_valid(id){
-        return (`${$(`#${id}`).val().toString()}` != '')
-    }
 
-    //How to optimize this?
-    if (value_id_valid('state')){
-        presense.state = value_id('state');
-    };
-    if (value_id_valid('details')){
-        presense.details = value_id('details');
-    }
-    if (value_id_valid('large_image')){
-        presense.assets.large_image = value_id('large_image');
-    }
-    if (value_id_valid('large_image_text')){
-        presense.assets.large_text = value_id('large_image_text');
-    }
-    if (value_id_valid('smal_image')){
-        presense.assets.small_image = value_id('smal_image');
-    }
-    if (value_id_valid('smal_image_text')){
-        presense.assets.small_text = value_id('smal_image_text');
-    }
-    if (value_id_valid('start_time')){
-        presense.timestamps.start = Number.parseInt(value_id('start_time'));
-    }else{
-        presense.timestamps.start = Math.round(new Date());
-    }
-    if (value_id_valid('end_time')){
-        presense.timestamps.end = Number.parseInt(value_id('end_time'));
-    }
-    rpc_client.request('SET_ACTIVITY', {
-        pid: process.pid,
-        activity: presense
+    $('#settings textarea').toArray().forEach(area => {
+        if (valid(area)){
+            if (area.id == 'state'){
+                presense.state = validator(area);
+            }else if (area.id == 'details'){
+                presense.details = validator(area);
+            }
+            else if (area.id == 'start_time'){
+                try{
+                    presense.timestamps.start = Number.parseInt(validator(area));
+                }catch(e){
+                    presense.timestamps.start = Math.round(new Date());
+                }
+            }
+            else if (area.id == 'end_time'){
+                try{
+                    let time = Number.parseInt(validator(area));
+                    presense.timestamps.end = time;
+                }catch(e){}
+            }
+            else if (area.id == 'large_image_asset'){
+                presense.assets.large_image = validator(area);
+            }
+            else if (area.id == 'large_image_text'){
+                presense.assets.large_text = validator(area);
+            }
+            else if (area.id == 'small_image_asset'){
+                presense.assets.small_image = validator(area);
+            }
+            else if (area.id == 'small_image_text'){
+                presense.assets.small_text = validator(area);
+            }
+        }
     })
+    if ($("#buttons").children("div").toArray().length > 0){
+        presense.buttons = [];
+        $("#buttons").children("div").toArray().forEach(element => {
+            var btn_label = element.children.item(1).value.toString().replace(/\s/g, "");
+            var btn_link = element.children.item(4).value.toString().replace(/\s/g, "");
+            if (btn_label != '' && btn_link != ''){
+                presense.buttons.push({label: btn_label, url: btn_link});
+            }
+        })
+        if (presense.buttons.length == 0){
+            delete presense.buttons;
+        }
+    };
+
+    return presense;
 }
 
-function on_connect(){
-    rpc_client.login({clientId: $('#appid').val().toString()}).then(() => {
-        $("#panel :input").toArray().forEach(element => element.disabled = false)
-    }).catch((error) => {
-        alert(`Invalid Connection!\nReason:\n${error}`);
+function set_presense(){
+    console.log(JSON.stringify(get_presence()));
+    rpc_client.on('ready', () => {
+        rpc_client.request('SET_ACTIVITY', {
+            pid: process.pid,
+            activity: get_presence()
+        }).catch((error) => {
+            alert(`Invalid Update!\nReason:\n${error}`);
+        });
+
+    })
+    rpc_client.login({clientId: $('#app_id').val().toString()})
+    .catch((error) => {
+        alert(`Invalid Update!\nReason:\n${error}`);
     });
 }
+
+
