ea8a154b1e196ef8d6026eb402d45af1fd19f612
Jansy-cc <77269679+Jansy-cc@users.noreply.github.com>
2021-02-07 21:08:15 +0200

    Discord Theme
    Change Color Theme To Discord

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const {app, BrowserWindow} = require('electron');
 let win;
 function createWindow(){
     win = new BrowserWindow({
-        width: 890,
-        height: 600,
+        width: 880,
+        height: 770,
         resizable: false,
         webPreferences: {
             nodeIntegration: true,
@@ -15,7 +15,7 @@ function createWindow(){
           transparent: true
     });
     win.loadURL(`file://${__dirname}/index.html`);
-    //win.setMenu(null);
+    win.setMenu(null);
     //win.webContents.openDevTools();
     
     win.on('closed', ()=>{
