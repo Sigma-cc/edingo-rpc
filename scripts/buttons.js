dc048fd9301a11c8a2b2458d94601ccc961cea0e
Jansy-cc <77269679+Jansy-cc@users.noreply.github.com>
2021-02-09 18:27:55 +0200

    Set Maximum Buttons Count

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,12 +1,17 @@
 function add_button(){
-    $("#buttons").append(`        <div id = "button">
+    if ($('#buttons').children('div').length + 1 < 3){
+      $("#buttons").append(`        <div id = "button" class = "discord">
     <label>Label:</label>
     <input placeholder="Text"><br>
     <label>Link:</label>
     <input placeholder="Link"><br>
     <button type="button" class="btn btn-outline-danger my-0 my-sm-0" onclick="remove_button(this)" id = "rpc-button">Remove</button><br>
   </div>
-  `);
+  `);}
+  else{
+    alert('You can install no more than 2 buttons.')
+  }
+
 }
 
 function remove_button(btn){
